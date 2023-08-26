5962fe6b0b52dbc0089db00344542825328e714e
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2023-08-25 22:55:51 -0500

    menu inicio

--- program.cpp
+++ program.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <locale.h>
 
 using namespace std;
 
 void program();
+void titulo(string titulo);
+void menu_inicio();
 
 int main(int args, char* argv[])
 {
@@ -11,7 +14,7 @@ int main(int args, char* argv[])
         if (argument == "--version") {
             std::cout << "MiPrograma v1.0" << std::endl;
         } else {
-            std::cout << "Argumento no reconocido." << std::endl;
+            std::cout << "Tal vez quisiste decir \"--version\"" << std::endl;
         }
     } else {
         program();
@@ -20,5 +23,41 @@ int main(int args, char* argv[])
 }
 
 void program(){
-    cout << "Hola mundo" << endl;
+    setlocale(LC_ALL, "Spanish");
+    titulo("CALCULO MIS NOTAS");
+    titulo("INICIO");
+    int opcion = 0;
+    while (opcion != 3){
+        menu_inicio();
+        cin >> opcion;
+        switch (opcion){
+            case 1:
+                titulo("AÑADIR UN CURSO");
+                break;
+            case 2:
+                titulo("VER DATOS");
+                break;
+            case 3:
+                titulo("SALIR");
+                break;
+            default:
+                system("cls");
+                cout << "Opcion no valida" << endl;
+                break;
+        }
+    }   
+}
+
+void titulo(string titulo){
+    system("cls");
+    cout << titulo << endl;
+    cout << "-------------------------------" << endl;
+}
+
+void menu_inicio(){
+    cout << "1. Añadir un curso" << endl;
+    cout << "2. Ver datos" << endl;
+    cout << "3. Salir" << endl;
+    cout << "-------------------------------" << endl;
+    cout << "Ingrese una opcion: ";
 }
